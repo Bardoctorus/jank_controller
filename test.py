dbb12ce68d2256992d787326fdd7f9517d99d9ef
bardoctorus <ian.m.buckley@gmail.com>
2021-07-03 21:38:11 +0200

    some code tidying, weird float conversion bug

--- test.py
+++ test.py
@@ -18,32 +18,49 @@ import pyxinput
 import time
 import sys
 
+COMPORT = 'COM4'
+
 MyVirtual = pyxinput.vController()
 # This reads the serial port of the RP2040, as long as it's closed in MU
 # turns
-ser = serial.Serial(
-                    'COM8',
+try:
+    ser = serial.Serial(
+                    'COM4',
                     115200,
                     timeout=0,
                     parity=serial.PARITY_EVEN,
                     rtscts = 1
                     )
-
+except:
+        print("cannot make serial object")
 
 while True:
-    s = ser.read(100)
+    try:
+        serialData = ser.read(100)
+    except:
+        print("Cannot Read from Serial")
 
     #parse it, get ruid of the extra crap
     # give it to the game controller.
 
+    # it seems like blank byte messages are sized 33 - in 64-bit systems at least.
+    if (sys.getsizeof(serialData) > 33):
+        serialDecoded = serialData.decode()
+        print('Serial Decoded: ', serialDecoded)
 
-    if (sys.getsizeof(s) > 33):
-        p = s.decode()
-        print (p)
+        try:
+            floatSerial = float(serialDecoded)
+        except:
+            print('Could not convert ', serialDecoded, ' to float, skipping')
+            continue
+
 
         # this errors out sometimes trying to convert the float
         #make it something like if (converts p  to float without an error) -> set the value else do nothing.
-        MyVirtual.set_value('AxisLx', float(p))
+        try:
+            MyVirtual.set_value('AxisLx', float(floatSerial))
+        except:
+            print('Could not set Thumbstick X value to ', floatSerial, ', skipping')
         # BIGGER PROBLEM it doesn't seem to register as movement at all, but it has worked before so huh?
         time.sleep(0.5)
         
